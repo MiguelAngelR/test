0f40202f669d20cd4cd19d94e8912546784d7812
MiguelAngelR <miguel.rios.alonso@outlook.com>
2021-03-23 16:20:04 -0600

    git

---
+++ src/components/Test.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1> Example desde TEST</h1>
+        <p> Text variable : {{ textVar }}</p>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+    data(){
+        return {
+            textVar:"Variable"
+        }
+    }
+})
+</script>
