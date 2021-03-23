d4f2e52ef9bb4c6909c93a57fccf16ee3915645b
MiguelAngelR <miguel.rios.alonso@outlook.com>
2021-03-23 16:20:55 -0600

    Cambio

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,7 +16,7 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- Add "scoped" attribute to limit CSS to this component only cambio en comentarios-->
 <style scoped>
 h3 {
   margin: 40px 0 0;
